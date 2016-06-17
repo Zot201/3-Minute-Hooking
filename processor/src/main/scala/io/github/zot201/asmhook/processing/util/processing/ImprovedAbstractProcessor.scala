886e94dcf27b72d788ac7d6b27cdba4aefebdb32
Zot201 <iZothf@gmail.com>
2016-06-17 19:23:20 +0900

    Minor improvement.

--- processor/src/main/scala/io/github/zot201/asmhook/processing/util/processing/ImprovedAbstractProcessor.scala
+++ processor/src/main/scala/io/github/zot201/asmhook/processing/util/processing/ImprovedAbstractProcessor.scala
@@ -1,15 +1,16 @@
 package io.github.zot201.asmhook.processing.util.processing
 
 import java.util
+import javax.annotation.processing.AbstractProcessor
 
 import scala.collection.JavaConversions._
 
-abstract class ImprovedAbstractProcessor extends javax.annotation.processing.AbstractProcessor {
+abstract class ImprovedAbstractProcessor extends AbstractProcessor {
 
   override def getSupportedAnnotationTypes: util.Set[String] = {
     val supported = getClass.getAnnotation(classOf[SupportedAnnotations])
     if (supported != null) {
-      super.getSupportedAnnotationTypes ++ supported.value.map(_.getName)
+      super.getSupportedAnnotationTypes ++ supported.value.view.map(_.getName)
     }
     else {
       super.getSupportedAnnotationTypes
