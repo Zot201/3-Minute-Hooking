51f329e858d2cf80278e58b07e6aaa0b21f7d583
Zot201 <iZothf@gmail.com>
2016-07-20 05:23:50 +0900

    Added GenericHooks.

---
+++ example/src/main/java/io/github/zot201/asmhook/test/example/GenericHooks.java
@@ -0,0 +1,23 @@
+package io.github.zot201.asmhook.test.example;
+
+import io.github.zot201.asmhook.annotation.Condition;
+import io.github.zot201.asmhook.annotation.HookInstance;
+import io.github.zot201.asmhook.annotation.InAdvance;
+import io.github.zot201.asmhook.annotation.parameter.Receiver;
+import io.github.zot201.asmhook.test.example.util.MyMod;
+import net.minecraft.enchantment.EnumEnchantmentType;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemSword;
+import net.minecraft.item.ItemTool;
+
+/** Added for comprehensiveness concerning generics */
+public class GenericHooks<T extends Item> {
+
+  @HookInstance static final GenericHooks<ItemSword> INSTANCE = new GenericHooks<>();
+
+  @InAdvance(end = Condition.IF_TRUE)
+  boolean canEnchantItem(@Receiver EnumEnchantmentType type, T i) {
+    return type == MyMod.MY_ENCH_TYPE && (i instanceof ItemSword || i instanceof ItemTool);
+  }
+
+}
--- processor/src/test/scala/io/github/zot201/asmhook/test/HookTest.scala
+++ processor/src/test/scala/io/github/zot201/asmhook/test/HookTest.scala
@@ -17,7 +17,7 @@ package io.github.zot201.asmhook.test
 
 import com.google.testing.compile.JavaFileObjects
 import io.github.zot201.asmhook.processing.AsmHookProcessor
-import io.github.zot201.asmhook.test.example.{HookExamples, MethodHookInstance}
+import io.github.zot201.asmhook.test.example.{GenericHooks, HookExamples, MethodHookInstance}
 import io.github.zot201.asmhook.test.util.Truths._
 import org.junit.Test
 
@@ -28,10 +28,20 @@ class HookTest {
   def ex[T](implicit tag: ClassTag[T]) =
     JavaFileObjects.forResource(s"${tag.runtimeClass.getName.replace('.', '/')}.java")
 
-  @Test def methodHookInstance(): Unit = {
+  lazy val methodHookInstance = Seq(ex[MethodHookInstance], ex[HookExamples])
+  val processor = new AsmHookProcessor
+
+  @Test def methodHookInstance_(): Unit = {
     assertAboutJavaSources
-      .that(Seq(ex[MethodHookInstance], ex[HookExamples]).asJava)
-      .processedWith(new AsmHookProcessor)
+      .that(methodHookInstance.asJava)
+      .processedWith(processor)
+      .compilesWithoutError()
+  }
+
+  @Test def genericHooks(): Unit = {
+    assertAboutJavaSource
+      .that(ex[GenericHooks[_]])
+      .processedWith(processor)
       .compilesWithoutError()
   }
 }
